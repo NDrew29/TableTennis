fb9ee61aea7b7ae0ad426edb7faae9612f85500c
NDrew29 <130081737+NDrew29@users.noreply.github.com>
2025-08-14 16:13:48 -0400

    Delete Match.cs

--- Match.cs
+++
@@ -1,6 +0,0 @@
-public class Match {
-    public Player Player1 { get; set; }
-    public Player Player2 { get; set; }
-    public int GamesToWin { get; set; }
-    public List<(int p1, int p2)> GameScores { get; set; } = new();
-}
