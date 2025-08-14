e07c56bc068c6db44d36ee10cd5812ec49bd656d
NDrew29 <130081737+NDrew29@users.noreply.github.com>
2025-08-14 16:13:30 -0400

    Delete GameSettings.cs

--- GameSettings.cs
+++
@@ -1,5 +0,0 @@
-public class GameSettings {
-    public int MaxScore { get; set; } = 11;
-    public int ServeInterval { get; set; } = 2;
-    public bool IsDeuce => Player1Score >= 10 && Player2Score >= 10;
-}
